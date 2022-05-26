04ba9edf38354b7880ee544b248791cc09dbe735
souhailamzeh <souhaila.mzeh06@gmail.com>
2022-05-26 14:32:09 +0100

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navigation/navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  email:any;
+  password:any;
+
+  constructor(public router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+
+
+  connect()
+{
+  let Email="radhwanrouihm"
+  let password="usausa"
+
+  if((this.email==Email)&&(this.password==password))
+  {
+    this.router.navigateByUrl('/home')
+
+  }
+  else
+  {
+    Swal.fire({
+      title: 'Error!',
+      text: 'Verifier vos informations',
+      icon: 'error',
+      confirmButtonText: 'ok'
+    })  }
+   }
+
+}
